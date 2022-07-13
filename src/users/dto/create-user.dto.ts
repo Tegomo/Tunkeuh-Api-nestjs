b1ea1a2700d53672ab89952edd0fc59d0c7d021a
Team Solutions <armand.tegomo@team-solutions.org>
2022-07-13 16:33:37 +0100

    user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,53 @@
+import { Prop } from '@nestjs/mongoose';
+import { IsString } from 'class-validator';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 
-export class CreateUserDto extends AuthCredentialsDto {}
+export class CreateUserDto extends AuthCredentialsDto {
+    @IsString()
+    name: string;
+  
+    @IsString()
+    phone: string;
+    
+    @IsString()
+    profile_url: string;
+    
+    @IsString()
+    driver_licence_number: string;
+    
+    @IsString()
+    driver_licence_front: string;
+    
+    @IsString()
+    driver_licence_back: string;
+    
+    @IsString()
+    card_id_number: string;
+    
+    @IsString()
+    card_id_front: string;
+    
+    @IsString()
+    card_id_back: string;
+    
+    @Prop({ required: true, default: false })
+    isActive!: boolean;
+    
+    @Prop({ required: true, default: false })
+    isSuperAdmin!: boolean;
+    
+    @IsString()
+    entreprise: string;
+    
+    @IsString()
+    staff: string;
+    
+    @IsString()
+    adresses: string;
+
+    @Prop({ default: Date.now })
+    createdAt!: Date;
+
+    @Prop({ default: Date.now })
+    updatedAt!: Date;
+}
