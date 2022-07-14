df9628e5422df3e077247889e4fc6101f4f10218
Team Solutions <armand.tegomo@team-solutions.org>
2022-07-14 16:56:56 +0100

    create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,48 +1,77 @@
 import { Prop } from '@nestjs/mongoose';
-import { IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { AuthCredentialsDto } from './auth-credentials.dto';
 
 export class CreateUserDto extends AuthCredentialsDto {
+
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     name: string;
   
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     phone: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     profile_url: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     driver_licence_number: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     driver_licence_front: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     driver_licence_back: string;
     
     @IsString()
+    @IsOptional()
+    @Prop({ required: false, default: null })
     card_id_number: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     card_id_front: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: false, default: null })
     card_id_back: string;
     
+    @IsOptional()
+    @IsBoolean ()
     @Prop({ required: true, default: false })
     isActive!: boolean;
     
+    @IsOptional()
+    @IsBoolean ()
     @Prop({ required: true, default: false })
     isSuperAdmin!: boolean;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: true, default: false })
     entreprise: string;
     
-    @IsString()
+    @IsOptional()
+    @IsBoolean ()
+    @Prop({ required: true, default: false })
     staff: string;
     
+    @IsOptional()
     @IsString()
+    @Prop({ required: true, default: false })
     adresses: string;
 
     @Prop({ default: Date.now })
