d55a3a6142b60942b7980cdc19583ad9be8965c3
Team Solutions <armand.tegomo@team-solutions.org>
2022-10-24 15:06:21 +0100

    find all CarCategories

--- src/car_categories/car_categories.service.ts
+++ src/car_categories/car_categories.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CarCategorie, CarCategorieDocument } from './car_categorie.schema';
 import { CreateCarCategoryDto } from './dto/create-car_category.dto';
 import { UpdateCarCategoryDto } from './dto/update-car_category.dto';
 
 @Injectable()
 export class CarCategoriesService {
-  create(createCarCategoryDto: CreateCarCategoryDto) {
-    return 'This action adds a new carCategory';
+
+  constructor(
+    @InjectModel(CarCategorie.name) private carCategorie: Model<CarCategorieDocument>
+  ) {}
+
+
+  async create(createCarCategoryDto: CreateCarCategoryDto) {
+    const carCategories = await this.carCategorie
+    .find({ role: '62ca9ec9738ee38481256ddd' })
+    .populate('carsubcategories'  , null, CarCategorie.name)
+    .populate('parentcategory'  , null, CarCategorie.name)
+    .where('published').equals(true);
+
+    return carCategories;
   }
 
   findAll() {
